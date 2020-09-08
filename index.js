06ee8fcf25555530ea24039b017b541823c6ceae
unknown <cqxg@yandex.ru>
2020-09-08 23:07:52 +0300

    feat: add main variables

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const app = () => {
+    const match = document.querySelector('.match');
+    const introScreen = document.querySelector('.intro');
+    const playBtn = document.querySelector('.intro button');
+    const pScore = document.querySelector('.player-score p');
+    const cScore = document.querySelector('.computer-score p');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+    const options = document.querySelectorAll('.options button');
+
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
     console.log('qq')
 };
 
